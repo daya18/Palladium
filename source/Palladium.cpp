3b2beee91228379a779de6901103ae9174340558
Daya Darsan <dayadarsan1802@gmail.com>
2024-01-13 18:21:40 +0530

    Add SDL, Create an SDL hello world program

--- source/Palladium.cpp
+++ source/Palladium.cpp
@@ -1,7 +1,37 @@
 ﻿#include <iostream>
 
-int main()
+#include <SDL2/SDL.h>
+
+int main ( int argc, char * args [] )
 {
-	std::cout << "Hello World!" << std::endl;
+	SDL_Init ( 0 );
+	
+	auto window { SDL_CreateWindow ( "Palladium", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0 ) };
+
+	bool quit { false };
+
+	while ( ! quit )
+	{
+		SDL_Event event;
+
+		while ( SDL_PollEvent ( &event ) )
+		{
+			switch ( event.type )
+			{
+			case SDL_WINDOWEVENT:
+				switch ( event.window.event )
+				{
+				case SDL_WINDOWEVENT_CLOSE: 
+					quit = true;
+					break;
+				}
+				break;
+			}
+		}
+	}
+	
+	SDL_DestroyWindow ( window );
+	SDL_Quit ();
+
 	return 0;
 }
